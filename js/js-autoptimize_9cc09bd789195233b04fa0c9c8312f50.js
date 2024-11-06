ab1e01c0b8880e57deef2a1efd9fd4c5fd764de1
LAPTOP-S93F8J7K\asus <suemeanmhrzn100@gmail.com>
2024-11-06 14:13:21 +0545

    design change

---
+++ js/js-autoptimize_9cc09bd789195233b04fa0c9c8312f50.js
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access /wp-content/cache/autoptimize/js/autoptimize_9cc09bd789195233b04fa0c9c8312f50.js
+on this server.</p>
+<hr>
+<address>Apache Server at themes.3rdwavemedia.com Port 80</address>
+</body></html>
